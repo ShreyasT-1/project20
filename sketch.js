a58b75926265653c78b7c4a199b4f8e0e20ab9f7
carrotman189 <92344737+carrotman189@users.noreply.github.com>
2021-11-25 12:15:36 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -101,7 +101,7 @@ function draw() {
 function createCash() {
   if (World.frameCount % 200 == 0) {
    // Modify the positions of cash 
-    var cash = createSprite(Math.round(random(50, 350),80, 20, 20));
+    var cash = createSprite(Math.round(random(0, widowWidth),80, 20, 20));
     cash.addImage(cashImg);
   cash.scale=0.12;
   cash.velocityY = 5;
@@ -114,7 +114,7 @@ function createDiamonds() {
   if (World.frameCount % 320 == 0) {
        // Modify the positions of diamonds 
 
-    var diamonds = createSprite(Math.round(random(50, 350),80, 20, 20));
+    var diamonds = createSprite(Math.round(random(0, widowWidth),80, 20, 20));
     diamonds.addImage(diamondsImg);
   diamonds.scale=0.03;
   diamonds.velocityY = 5;
@@ -127,7 +127,7 @@ function createJwellery() {
   if (World.frameCount % 410 == 0) {
     //   Modify the positions of jwellery to make them spawn throughout the available screen size.
 
-    var jwellery = createSprite(Math.round(random(50, 350),80, 20, 20));
+    var jwellery = createSprite(Math.round(random(0, widowWidth),80, 20, 20));
     jwellery.addImage(jwelleryImg);
   jwellery.scale=0.13;
   jwellery.velocityY = 5;
@@ -140,7 +140,7 @@ function createSword(){
   if (World.frameCount % 530 == 0) {
     //   Modify the positions of sword to make them spawn throughout the available screen size.
 
-    var sword = createSprite(Math.round(random(50, 350),80, 20, 20));
+    var sword = createSprite(Math.round(random(0, widowWidth),80, 20, 20));
     sword.addImage(swordImg);
   sword.scale=0.1;
   sword.velocityY = 4;
